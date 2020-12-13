4f37850892c38dfa52c12e6c8f7ba4407005d5af
systemGene <65501694+systemGene@users.noreply.github.com>
2020-12-13 14:18:39 +0800

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -2,17 +2,29 @@
 	const client = new discord.Client();
 	let condo = "No condo detected!	"
 	const prefix = '!'
-
+	const cooldown = new Set();
 	client.on('ready', async () => {
 	    console.log(`ready`)
+	    client.user.setActivity('https://discord.gg/TyVpuDzqEH', {
+	    	type: 'PLAYING'
+	    }).catch(console.error);
 	})
 
 	client.on("message", message => {
-	  if (message.author.bot) return;
-	    if (message.content == "!condo") {
-	 	message.author.send("```css\nPossible condo sent.\n If this is banned wait a few minutes to change.```\n"+ condo +"")
-		message.react("🥵")
-	    }
+	  	if (message.author.bot) return;
+		if (message.content == "!condo") {
+			if(cooldown.has(message.author.id)){
+				message.reply("Cooldown! 10 seconds.")
+			} else {
+				message.author.send("```If the condo is banned wait a few minutes or seconds for it to update.```\n"+ condo +"")
+				message.react("✅")
+				message.reply("Check your DMS!")
+				cooldown.add(message.author.id);
+				setTimeout(() => {
+					cooldown.delete(message.author.id)
+				}, 10000)
+			}
+	   	}
 	});
 
 	client.on("message", message => {
@@ -21,24 +33,33 @@
 		const command = args.shift().toLowerCase();
 		if(message.channel.id === "786820507805679696"){
 			if(command === "post"){
-				if(!args.length){
-					return message.channel.send("Please provide a condo link.")
-				}
-				let checkURL = "roblox.com/games/60"
-				if (message.content.search(checkURL) >= 8){
-					let one9 = "786820507805679696"
-					client.channels.get(one9).send(""+ args[0] +"")
+				if(cooldown.has(message.author.id)){
+					message.reply("Cooldown! 10 seconds.")
+				} else {
+					if(!args.length){
+						return message.channel.send("Please provide a condo link.")
+					}
+					let checkURL = "roblox.com/games/60"
+					if (message.content.search(check3) >= 8){
+						let one1 = "787073474219737088"
+						let one2 = "782053586175655938"
+						client.channels.get(one1).send(""+ args[0] +"")
+						client.channels.get(one2).send(""+ args[0] +"")
+						client.channels.get(sender).send("Successfully sent condo link to 2 CONDO SERVERS")
+					}	
+					cooldown.add(message.author.id);
+					setTimeout(() => {
+						cooldown.delete(message.author.id)
+					}, 10000)
 				}
 			}
 		}
 	})
 
 	client.on("message", message => {
-		let reply = ""
 		let check3 = "roblox.com/games/60"
 		let check4 = "roblox.com/games/59"
-		if (message.author.bot) return;	
-
+		if (message.author.bot) return;
 		if (message.channel.id === "781880694031777802"){ // CALI SERVER
 			message.content = message.content.toLowerCase();
 			var randomColor = Math.floor(Math.random()*16777215).toString(16);
@@ -51,8 +72,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Cali Condos | https://discord.gg/TzQ5Dce5Vn")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "777206202916405278"){ // COMMUNITY SERVER
@@ -67,8 +86,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Community Condos | https://discord.gg/TzQ5Dce5Vn")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "783924733522608179"){ // CASHOUT SERVER
@@ -83,8 +100,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("CA$HOUT | https://discord.gg/BrsucuvDhu")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "759244053517697044"){ // THOTCON SERVER
@@ -99,8 +114,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Thot Con | discord.gg/thotcon")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "784196552331624468"){ // CONDO REBORN SERVER
@@ -115,8 +128,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Condo Reborn | https://discord.gg/vbNnqVmJdF")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "783685693896785942"){ // CONDO SERVER
@@ -131,8 +142,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Condo | https://discord.gg/mJYkKuXB7X")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "756223026324045934"){ // PARADISE SERVER
@@ -147,8 +156,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Paradise Condos |https://discord.gg/gYQkbAgpdQ")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "770032561841373194"){ // FUTURE SERVER
@@ -163,8 +170,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Future | https://discord.gg/eWPQkQWebF")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 		if (message.channel.id === "772365886115282944"){ // MIDNIGHT SOCIETY SERVER
@@ -179,8 +184,6 @@
 				.setDescription("**Condo:** ("+ message.content +")\n **Thank you xkid#1527 for help!**")
 				.setFooter("Midnight Society | https://discord.gg/sdunWEHF")
 				client.channels.get(sender).send({ embed: embed })
-				message.react("✅")
-				message.react("❌")
 			}
 		}
 	})
