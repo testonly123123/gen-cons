a3654abe842f56011b46935b8dd6e2d311faeb63
systemGene <65501694+systemGene@users.noreply.github.com>
2021-03-20 22:25:42 +0800

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -9,7 +9,7 @@ client.on('ready', async () => {
 })
 
 client.on("message", message => {
-	let check = "roblox.com/games/64"
+	let check = "roblox.com/games/65"
 	if(message.author.bot) return
 	if(message.content === ",condo"){
 		if(cooldown.has(message.author.id)){
