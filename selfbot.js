2ad39154f3a152eac9a9c26849f97630a1954934
systemGene <65501694+systemGene@users.noreply.github.com>
2021-03-21 21:02:15 +0800

    Update selfbot.js

--- selfbot.js
+++ selfbot.js
@@ -22,7 +22,7 @@ client.on("message", message => {
 		}
 	}
 
-	if(message.channel.id === "812038769506516993" || message.channel.id === "821094121141043210" || message.channel.id === "792301294005649448" || message.channel.id === "801701275069579324" || message.channel.id === "807768141811023952" || message.channel.id === "801701275069579324" || message.channel.id === "771975903684329513" || message.channel.id === "777967074269986866" || message.channel.id === "771975903684329513" || message.channel.id === "748510864814112799" || message.channel.id === "777967074269986866" || message.channel.id === "795488305805393923" || message.channel.id === "804163586896822303"){
+	if(message.channel.id === "814627552945504326" || message.channel.id === "806033318251593728" || message.channel.id === "814525962996940800" || message.channel.id === "790770894691631135" || message.channel.id === "817172699502870588" || message.channel.id === "803916220880912414" || message.channel.id === "792530975959679017" || message.channel.id === "775006995685572638" || message.channel.id === "799459461407965184" || message.channel.id === "812038769506516993" || message.channel.id === "792776632481218571" || message.channel.id === "822642152886108170" || message.channel.id === "792301294005649448" || message.channel.id === "801701275069579324" || message.channel.id === "807768141811023952" || message.channel.id === "771975903684329513" || message.channel.id === "804163586896822303" || message.channel.id === "795488305805393923" || message.channel.id === "777967074269986866"){
 		message.content = message.content.toLowerCase();
 		var randomColor = Math.floor(Math.random()*16777215).toString(16);
 		let sender = "809420315230339112"
